89e94c542958427e7bdc1861e986a665fd1f8095
Gagancoderai <gaganhalati@gmail.com>
2025-11-11 14:35:56 +0530

    Create voweiorconsonant.py

---
+++ voweiorconsonant.py
@@ -0,0 +1,9 @@
+# Character Type Checker Write a Python program to check whether a given character is a vowel or a consonant.
+b = (input("enter the character"))
+if b in"aeiou":
+    print("vowel")
+else:
+    print("consonant")   
+out put
+enter the character e
+consonant
