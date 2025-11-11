f43ee2140cfa76c126bffb01a9a1ff237bcaea27
Gagancoderai <gaganhalati@gmail.com>
2025-11-11 19:31:19 +0530

    Create pass example.py

---
+++ pass example.py
@@ -0,0 +1,24 @@
+# Pass Statement Example
+print("Demonstrating the 'pass' statement:")
+for i in range(1, 11):
+    # This 'pass' statement does nothing, it's just a placeholder
+    pass
+    print(f"Current number: {i}")
+
+print("\nIn this example, 'pass' allowed the loop to continue without any specific action when iterating.")
+print("It's useful when syntax requires a statement but no action is desired (e.g., in an empty function or conditional block).")
+out put
+Demonstrating the 'pass' statement:
+Current number: 1
+Current number: 2
+Current number: 3
+Current number: 4
+Current number: 5
+Current number: 6
+Current number: 7
+Current number: 8
+Current number: 9
+Current number: 10
+
+In this example, 'pass' allowed the loop to continue without any specific action when iterating.
+It's useful when syntax requires a statement but no action is desired (e.g., in an empty function or conditional block).
