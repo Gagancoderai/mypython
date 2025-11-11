50471c83785a0579aab52e7e005936ce5051f85c
Gagancoderai <gaganhalati@gmail.com>
2025-11-11 14:30:55 +0530

    Create leapyearornot.py

---
+++ leapyearornot.py
@@ -0,0 +1,9 @@
+# Leap Year Checker Write a Python program to check whether a given year is a leap year or not.
+year = int(input("enter the year:"))
+if year%4==0:
+    print("leap year")
+else:
+    print("not a leap year")
+out put
+enter the year:2024
+leap year
