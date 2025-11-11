93f69b4fbe97d507ba85c0e7a6dc534d38f03474
Gagancoderai <gaganhalati@gmail.com>
2025-11-11 14:24:21 +0530

    Create if_elif_else.py

---
+++ if_elif_else.py
@@ -0,0 +1,16 @@
+# find the mark  using if_elif_eles condition
+mark = int(input("enter the mark:"))
+if mark>= 90:
+    print("very good")
+elif mark>=75:
+    print("good")
+elif mark>=60:
+    print("average")
+elif mark>=50:
+    print("just pass")
+else:
+    print("fail")
+out put
+enter the mark: 73
+average
+
