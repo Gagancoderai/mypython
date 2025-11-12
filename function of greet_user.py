e7d8053f16243507680940d5ced5155ff8eea027
Gagancoderai <gaganhalati@gmail.com>
2025-11-12 20:22:23 +0530

    Create function of greet_user.py

---
+++ function of greet_user.py
@@ -0,0 +1,9 @@
+#Write a function greet_user() that takes a name as an argument and prints a greeting message like “Hello, Rajesh! Welcome to Python.”
+def greet_user(name):
+    """Takes a name as an argument and prints a greeting message."""
+    print(f"Hello, {name}! Welcome to Python.")
+greet_user("Gagan")
+greet_user("Madan")
+out put
+Hello, Gagan! Welcome to Python.
+Hello, Madan! Welcome to Python.
