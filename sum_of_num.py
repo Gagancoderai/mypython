7d75f7a22bf914fb291d0e4e5e4dc7d6b9abf495
Gagancoderai <gaganhalati@gmail.com>
2025-11-12 15:50:24 +0530

    Create sum_of_num.py

---
+++ sum_of_num.py
@@ -0,0 +1,9 @@
+# Write a Python function called add_numbers() that takes two numbers as arguments and returns their sum.
+def add_numbers(num1, num2):
+    return num1 + num2
+
+# Example usage:
+result = add_numbers(5, 7)
+print(f"The sum is: {result}") 
+out put
+The sum is: 12
