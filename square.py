d24aae95b1ecd222efd7027ff816b6c8f19d737a
Gagancoderai <gaganhalati@gmail.com>
2025-11-12 15:54:26 +0530

    Create square.py

---
+++ square.py
@@ -0,0 +1,9 @@
+# Write a function find_square() that accepts a number and returns its square.
+def find_square(number):
+    return number * number
+# Example usage:
+num = 7
+square_of_num = find_square(num)
+print(f"The square of {num} is: {square_of_num}")
+ Out put
+The square of 7 is: 49
