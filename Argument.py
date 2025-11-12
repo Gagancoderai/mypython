1fd728fdcd04aa3383adcbb6eb983ffcae965e76
Gagancoderai <gaganhalati@gmail.com>
2025-11-12 15:44:38 +0530

    Create Argument.py

---
+++ Argument.py
@@ -0,0 +1,22 @@
+#position argument
+def add(age, name, roll):
+    print(age)
+    print(name)
+    print(roll)
+add(18,"gagan",12)   
+OUT PUT
+18
+gagan
+12
+#key argument
+def add(a,b):
+    return a+b
+add(b=3,a=2)
+OUT PUT
+5
+#Default argument.
+def add(a=1,b=2):
+  return a+b
+add()
+OUT PUT
+3
