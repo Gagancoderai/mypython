cb1e5c9ec1dbfd692d6e9a45d6811e0a9a220d44
Gagancoderai <gaganhalati@gmail.com>
2025-11-12 10:20:26 +0530

    Create formatted string example.py

---
+++ formatted string example.py
@@ -0,0 +1,8 @@
+from re import MULTILINE
+name = "gagan"
+age = 17
+college = "SIET"
+branch = "AI&Ml"
+print(f"name is {name} and my age is {age} i am studying at {college} in the branch of {branch} ")
+out put
+name is gagan and my age is 17 i am studying at SIET in the branch of AI&Ml 
