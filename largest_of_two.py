1a7bd84c99957549b2be588e3e723c48c670a0ed
Gagancoderai <gaganhalati@gmail.com>
2025-11-12 16:19:45 +0530

    Create largest_of_two.py

---
+++ largest_of_two.py
@@ -0,0 +1,6 @@
+# Write a function largest_of_two() that accepts two numbers and prints which one is larger.
+def largest_of_two(num1,num2):
+    return(num1>num2)
+largest_of_two(5,2)    
+ Out put
+ true
