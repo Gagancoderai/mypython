562105bcb0d8fbb2e8ddb3ac0f9374b434eefe4e
Gagancoderai <gaganhalati@gmail.com>
2025-11-15 11:44:39 +0530

    Create class.py

---
+++ class.py
@@ -0,0 +1,9 @@
+class circle:
+    def __init__(self,radius):
+        self.radius=radius
+    def area(self):
+        return 3.14*self.radius*self.radius
+obj=circle(2)
+obj.area()    
+out put
+12.56
