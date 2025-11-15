437eaf3b1d7e4621248b777fd141eab0d89f7e37
Gagancoderai <gaganhalati@gmail.com>
2025-11-15 11:51:27 +0530

    Create multiple.py

---
+++ multiple.py
@@ -0,0 +1,15 @@
+class A:
+  def fun1(self):
+    print("day 01")
+class B:
+  def fun02(self):
+    print("day 02")
+class C(A,B):
+  pass
+obj = A()
+obj2 = B()
+obj.fun1()
+obj2.fun02() 
+out put 
+day 01
+day 02
