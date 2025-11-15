c6b815bfe6dbbea3a756032aa8c93a5f7b6a6a5f
Gagancoderai <gaganhalati@gmail.com>
2025-11-15 11:59:37 +0530

    Create polly.py

---
+++ polly.py
@@ -0,0 +1,11 @@
+class bird:
+    def speak(self):
+        print("i chrip")
+class parrot(bird):
+    def speak(self):
+        print("fill good")
+obj = parrot()
+obj.speak()   
+out put
+fill good
+
